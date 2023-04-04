ad73a3d7f6eec30b10e55589a6ff0a6e1b1964e5
Gweltaz <gweltazmary@gmail.com>
2023-04-05 00:41:19 +0200

    DropDownMenu props and logic

--- src/Components/about/AboutHeroBanner.jsx
+++ src/Components/about/AboutHeroBanner.jsx
@@ -3,9 +3,13 @@ import heroBannerPicture from '../../assets/about_banner.png';
 
 export default function AboutHeroBanner() {
 	return (
-		<section class='top-banner-section'>
-			<div class='banner-image-div'>
-				<img class='banner-image' src={heroBannerPicture} alt='Banner Image' />
+		<section className='top-banner-section'>
+			<div className='banner-image-div'>
+				<img
+					className='banner-image'
+					src={heroBannerPicture}
+					alt='Banner Image'
+				/>
 			</div>
 		</section>
 	);
--- src/Components/home/HomeHeroBanner.jsx
+++ src/Components/home/HomeHeroBanner.jsx
@@ -4,15 +4,19 @@ import heroBannerPicture from '../../assets/home_banner.png';
 export default function HomeHeroBanner() {
 	return (
 		// The CSS grid that makes up the entirety of the hero image/banner image area
-		<section class='top-banner-section'>
+		<section className='top-banner-section'>
 			{/* The CSS grid area that displays the image (layer 1) */}
-			<div class='banner-image-div'>
-				<img class='banner-image' src={heroBannerPicture} alt='Banner Image' />
+			<div className='banner-image-div'>
+				<img
+					className='banner-image'
+					src={heroBannerPicture}
+					alt='Banner Image'
+				/>
 			</div>
 			{/* The CSS grid area that displays the semi-transparent gradient overlay (layer 2)*/}
-			<div class='banner-overlay-div'></div>
+			<div className='banner-overlay-div'></div>
 			{/* The CSS grid area that displays the content (layer 3)*/}
-			<div class='banner-text-div'>
+			<div className='banner-text-div'>
 				<h1>Chez vous, partout et ailleurs</h1>
 			</div>
 		</section>
