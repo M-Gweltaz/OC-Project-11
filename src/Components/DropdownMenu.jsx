1793d7f4e43458a35758d978cf4482e25f970999
Gweltaz <gweltazmary@gmail.com>
2023-04-19 09:50:37 +0200

    small fixes

--- src/Components/DropdownMenu.jsx
+++ src/Components/DropdownMenu.jsx
@@ -13,54 +13,30 @@ export default function DropDownMenu({ title, text, list, width }) {
 
 	return (
 		<>
-			{isMenuOpen ? (
-				<section
-					className='DropDownMenu DropDownMenuOpen'
-					style={{ width: width }}
-				>
-					{' '}
-					<div className='DropDownMenu_title' onClick={handleClick}>
-						<h3>{title}</h3>
-						{isMenuOpen ? (
-							<img src={arrowDropUp} alt='menu déroulant' />
-						) : (
-							<img src={arrowDropDown} alt='menu déroulant' />
-						)}
-					</div>
-					{text && isMenuOpen && (
-						<div className='DropDownMenu_text'>{text}</div>
+			<section
+				className={
+					isMenuOpen
+						? `DropDownMenu DropDownMenuOpen ${width}`
+						: `DropDownMenu ${width}`
+				}
+			>
+				<div className='DropDownMenu_title' onClick={handleClick}>
+					<h3>{title}</h3>
+					{isMenuOpen ? (
+						<img src={arrowDropUp} alt='menu déroulant' />
+					) : (
+						<img src={arrowDropDown} alt='menu déroulant' />
 					)}
-					{list && isMenuOpen && list.length > 0 && (
-						<ul className='DropDownMenu_list'>
-							{list.map((item) => (
-								<li key={item}>{item}</li>
-							))}
-						</ul>
-					)}
-				</section>
-			) : (
-				<section className='DropDownMenu' style={{ width: width }}>
-					{' '}
-					<div className='DropDownMenu_title' onClick={handleClick}>
-						<h3>{title}</h3>
-						{isMenuOpen ? (
-							<img src={arrowDropUp} alt='menu déroulant' />
-						) : (
-							<img src={arrowDropDown} alt='menu déroulant' />
-						)}
-					</div>
-					{text && isMenuOpen && (
-						<div className='DropDownMenu_text'>{text}</div>
-					)}
-					{list && isMenuOpen && list.length > 0 && (
-						<ul className='DropDownMenu_list'>
-							{list.map((item) => (
-								<li key={item}>{item}</li>
-							))}
-						</ul>
-					)}
-				</section>
-			)}
+				</div>
+				{text && isMenuOpen && <div className='DropDownMenu_text'>{text}</div>}
+				{list && isMenuOpen && list.length > 0 && (
+					<ul className='DropDownMenu_list'>
+						{list.map((item) => (
+							<li key={item}>{item}</li>
+						))}
+					</ul>
+				)}
+			</section>
 		</>
 	);
 }
--- src/Components/rental/RatingRental.jsx
+++ src/Components/rental/RatingRental.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import starRating from '../../assets/star_rating.png';
 import starActiveRating from '../../assets/star_active_rating.png';
 import '../../style/Rental/RatingRental.css';
@@ -6,61 +5,161 @@ import '../../style/Rental/RatingRental.css';
 export default function RatingRental({ rating }) {
 	let ratingRender;
 	switch (true) {
-		case rating == '1':
+		case rating === '1':
 			ratingRender = (
 				<div className='ratingsContainer'>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
 				</div>
 			);
 			break;
-		case rating == '2':
+		case rating === '2':
 			ratingRender = (
 				<div className='ratingsContainer'>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
 				</div>
 			);
 			break;
 
-		case rating == '3':
+		case rating === '3':
 			ratingRender = (
 				<div className='ratingsContainer'>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
 				</div>
 			);
 			break;
 
-		case rating == '4':
+		case rating === '4':
 			ratingRender = (
 				<div className='ratingsContainer'>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
 				</div>
 			);
 			break;
 
-		case rating == '5':
+		case rating === '5':
 			ratingRender = (
 				<div className='ratingsContainer'>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
-					<img src={starActiveRating} className='ratingStar'></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starActiveRating}
+						alt='active rated star'
+						className='ratingStar'
+					></img>
 				</div>
 			);
 			break;
@@ -68,11 +167,31 @@ export default function RatingRental({ rating }) {
 		default:
 			ratingRender = (
 				<div className='ratingsContainer'>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
-					<img src={starRating} className='ratingStar'></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
+					<img
+						src={starRating}
+						alt='empty rated star'
+						className='ratingStar'
+					></img>
 				</div>
 			);
 			break;
